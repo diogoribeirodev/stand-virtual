fcd5202771e1a472ebac8f35ecec894893a6bf91
Diogo Ribeiro <me@diogoribeiro.dev>
2023-11-30 17:07:59 +0000

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ import '@ionic/react/css/text-alignment.css';
 import '@ionic/react/css/text-transformation.css';
 
 /* Theme variables */
+//import './index.css';
 import './theme/variables.css';
 
 setupIonicReact();
