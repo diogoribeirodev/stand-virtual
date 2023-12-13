8c5a80e2d51bf263e321a84705d480e012fd4d14
danielkozlov11 <danielkozlovr@gmail.com>
2023-12-13 23:56:20 +0000

    update (home, data)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,56 +1,116 @@
-import { Geolocation } from '@capacitor/geolocation';
-import { useEffect, useState } from 'react';
-import { stores } from '../data';
+import { Geolocation } from "@capacitor/geolocation";
+import { useEffect, useState } from "react";
+import { stores, new_cars } from "../data";
+import {
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonImg,
+} from "@ionic/react";
 
 export const Home: React.FC = () => {
+  const [currentPosition, setCurrentPosition] = useState({
+    latitude: 0,
+    longitude: 0,
+  });
 
-    const [currentPosition, setCurrentPosition] = useState({ latitude: 0, longitude: 0 });
+  const printCurrentPosition = async () => {
+    const { latitude, longitude } = (await Geolocation.getCurrentPosition())
+      .coords;
+    setCurrentPosition({ latitude, longitude });
+  };
 
-    const printCurrentPosition = async () => {
-        const { latitude, longitude } = (await Geolocation.getCurrentPosition()).coords;
-        setCurrentPosition({ latitude, longitude });
-    };
-
-    useEffect(() => {
-        printCurrentPosition();
-        for (let i = 0; i < stores.length; i++) {
-            stores[i].id = distance({
-                lat1: currentPosition.latitude,
-                lon1: currentPosition.longitude,
-                lat2: stores[i].latitude,
-                lon2: stores[i].longitude,
-                unit: "K"
-            });
-        }
-    }, []);
-
-    return (
-        <span>
-            <h1>Home</h1>
-            <p>Latitude: {currentPosition.latitude}</p>
-            <p>Longitude: {currentPosition.longitude}</p>
-        </span>
-    );
-}
-
-function distance({ lat1, lon1, lat2, lon2, unit }: { lat1: number, lon1: number, lat2: number, lon2: number, unit: string }) {
-    if ((lat1 == lat2) && (lon1 == lon2)) {
-        return 0;
+  useEffect(() => {
+    printCurrentPosition();
+    for (let i = 0; i < stores.length; i++) {
+      stores[i].id = distance({
+        lat1: currentPosition.latitude,
+        lon1: currentPosition.longitude,
+        lat2: stores[i].latitude,
+        lon2: stores[i].longitude,
+        unit: "K",
+      });
     }
-    else {
-        var radlat1 = Math.PI * lat1 / 180;
-        var radlat2 = Math.PI * lat2 / 180;
-        var theta = lon1 - lon2;
-        var radtheta = Math.PI * theta / 180;
-        var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-        if (dist > 1) {
-            dist = 1;
-        }
-        dist = Math.acos(dist);
-        dist = dist * 180 / Math.PI;
-        dist = dist * 60 * 1.1515;
-        if (unit == "K") { dist = dist * 1.609344 }
-        if (unit == "N") { dist = dist * 0.8684 }
-        return dist;
+  }, []);
+
+  return (
+    <span>
+      <IonCard>
+        <IonCardHeader>
+          <IonCardSubtitle>Localização</IonCardSubtitle>
+        </IonCardHeader>
+        <IonCardContent>
+          Latitude: {currentPosition.latitude}/ Longitude:{" "}
+          {currentPosition.longitude}
+        </IonCardContent>
+      </IonCard>
+      <h3>
+        <b></b>HAPPY CAR RENT
+      </h3>
+      <IonImg
+        src="https://docs-demo.ionic.io/assets/madison.jpg"
+        alt="The Wisconsin State Capitol building in Madison, WI at night"
+      ></IonImg>
+
+      <p>
+        <h4>
+          {" "}
+          A "Happy Car Rent" é uma empresa de aluguer de veículos que se destaca
+          pela sua presença global, oferecendo uma ampla variedade de opções de
+          aluguer em várias localidades ao redor do mundo. Com o compromisso de
+          proporcionar uma experiência de condução inigualável aos nossos
+          clientes, a "Happy Car Rent" tem expandido constantemente a sua rede
+          de lojas para garantir que você possa encontrar os veículos perfeitos
+          onde quer que a sua jornada o leve. Nossas lojas estão
+          estrategicamente localizadas em destinos populares, aeroportos
+          movimentados e centros urbanos, tornando fácil e conveniente o acesso
+          aos nossos serviços de aluguer de carros de alta qualidade. Na "Happy
+          Car Rent", acreditamos que a mobilidade é a chave para explorar novos
+          horizontes, e é por isso que nos esforçamos para fornecer uma frota
+          diversificada que atenda a todas as suas necessidades de viagem.
+        </h4>
+      </p>
+    </span>
+  );
+};
+
+function distance({
+  lat1,
+  lon1,
+  lat2,
+  lon2,
+  unit,
+}: {
+  lat1: number;
+  lon1: number;
+  lat2: number;
+  lon2: number;
+  unit: string;
+}) {
+  if (lat1 == lat2 && lon1 == lon2) {
+    return 0;
+  } else {
+    var radlat1 = (Math.PI * lat1) / 180;
+    var radlat2 = (Math.PI * lat2) / 180;
+    var theta = lon1 - lon2;
+    var radtheta = (Math.PI * theta) / 180;
+    var dist =
+      Math.sin(radlat1) * Math.sin(radlat2) +
+      Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    if (dist > 1) {
+      dist = 1;
     }
+    dist = Math.acos(dist);
+    dist = (dist * 180) / Math.PI;
+    dist = dist * 60 * 1.1515;
+    if (unit == "K") {
+      dist = dist * 1.609344;
+    }
+    if (unit == "N") {
+      dist = dist * 0.8684;
+    }
+    return dist;
+  }
 }
