1396cd9e205490bcd85b4efa032e055c549c185d
danielkozlov11 <danielkozlovr@gmail.com>
2023-12-13 15:19:47 +0000

    update (cars, variable.css, explorecontainer.css)

--- src/pages/Cars.tsx
+++ src/pages/Cars.tsx
@@ -4,51 +4,76 @@ import { IonItem, IonLabel, IonList, IonListHeader } from "@ionic/react";
 export const Cars: React.FC = () => {
   return (
     <>
-      <IonList>
-        {" "}
-        <IonListHeader>
-          <IonLabel>UTILITÁRIOS</IonLabel>
-        </IonListHeader>
-        <IonItem>
-          <IonLabel>Citroen C3</IonLabel>
-        </IonItem>
-        <IonItem>
-          <IonLabel>Ford Fiesta</IonLabel>
-        </IonItem>
-        <IonItem>
-          <IonLabel>Peugeot 208</IonLabel>
-        </IonItem>
-        <IonItem>
-          <IonLabel>Renault Clio</IonLabel>
-        </IonItem>
-        <IonItem>
-          <IonLabel>Volkswagen Polo</IonLabel>
-        </IonItem>
-        <IonItem>
-          <IonLabel>Seat Ibiza</IonLabel>
-        </IonItem>
-        <IonItem>
-          <IonLabel>Audi A1</IonLabel>
-        </IonItem>
-        <IonItem>
-          <IonLabel>Mercedes-Benz Classe A</IonLabel>
-        </IonItem>
-        <IonItem>
-          <IonLabel>Nissan Micra</IonLabel>
-        </IonItem>
-      </IonList>
-      <IonList>
-        <IonListHeader>
-          <IonLabel>SUV</IonLabel>
-        </IonListHeader>
-        <IonItem>
-          <IonLabel>Item</IonLabel>
-        </IonItem>
-        <IonItem>
-          <IonLabel>Item</IonLabel>
-        </IonItem>
-      </IonList>
-      ;
+      <div id="main">
+        <IonList>
+          {" "}
+          <IonListHeader>
+            <IonLabel>UTILITÁRIOS</IonLabel>
+          </IonListHeader>
+          <IonItem>
+            <IonLabel>Citroen C3</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Ford Fiesta</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Peugeot 208</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Renault Clio</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Volkswagen Polo</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Seat Ibiza</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Audi A1</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Mercedes-Benz Classe A</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Nissan Micra</IonLabel>
+          </IonItem>
+        </IonList>
+        <IonList>
+          <IonListHeader>
+            <IonLabel>SUV</IonLabel>
+          </IonListHeader>
+          <IonItem>
+            <IonLabel>Volkswagen Tiguan</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Mazda CX-5</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Hyundai Kauai</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>KIA Sportage</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Peugeot 3008</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Jaguar E-Pace</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Porsche Macan</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Mercedes GLC</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Tesla Model X</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Mazda MX 30</IonLabel>
+          </IonItem>
+        </IonList>
+      </div>
     </>
   );
 };
