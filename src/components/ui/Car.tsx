26b4b8e4620ef9829d8a26d8277ca2524d74ca94
danielkozlov11 <danielkozlovr@gmail.com>
2023-12-13 22:22:41 +0000

    update car

--- src/components/ui/Car.tsx
+++ src/components/ui/Car.tsx
@@ -1,4 +1,14 @@
 import React from "react";
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonContent,
+  useIonModal,
+} from "@ionic/react";
 interface CarProps {
   id: number;
   name: string;
@@ -10,6 +20,32 @@ interface CarProps {
   combustible: string;
   store_id: string;
 }
+
+function Cars({ car }: { car: CarProps }) {
+  const [present] = useIonModal(Modal);
+
+  return (
+    <>
+      <IonCard color="light">
+        <img
+          alt="Silhouette of mountains"
+          src="https://ionicframework.com/docs/img/demos/card-media.png"
+        />
+        <IonCardHeader>
+          <IonCardTitle>{car.name}</IonCardTitle>
+          <IonCardSubtitle>{car.category}</IonCardSubtitle>
+        </IonCardHeader>
+        <IonCardContent>
+          <IonButton fill="clear" onClick={() => present()} expand="block">
+            View More
+          </IonButton>
+        </IonCardContent>
+      </IonCard>
+    </>
+  );
+}
+export default Cars;
+
 export function Car({ car }: { car: CarProps }) {
   return (
     <div>
@@ -17,3 +53,7 @@ export function Car({ car }: { car: CarProps }) {
     </div>
   );
 }
+
+const Modal: React.FC = () => {
+  return <IonContent>p</IonContent>;
+};
