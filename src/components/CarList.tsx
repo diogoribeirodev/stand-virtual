f91e57da625e90234e5b26b5e6c5a2d70184ed4b
Diogo Ribeiro <me@diogoribeiro.dev>
2023-12-13 23:57:40 +0000

    ...

--- src/components/CarList.tsx
+++ src/components/CarList.tsx
@@ -1,14 +1,56 @@
-import { IonCol, IonGrid, IonRow } from "@ionic/react";
+import { IonCol, IonGrid, IonItem, IonList, IonRow, IonSelect, IonSelectOption } from "@ionic/react";
 import React from "react";
+import { useParams } from "react-router";
 import { cars } from "../data";
 import Car from "./ui/Car";
 
 export const CarList: React.FC = () => {
+  const [tipo, setTipo] = React.useState<string>('');
+  const [combustivel, setCombustivel] = React.useState<string>('');
+  const storeId = useParams();
+
+  const filteredCars = cars.filter(car => {
+    if (storeId.id && car.store_id !== parseInt(storeId.id)) {
+      return false;
+    }
+    if (tipo && car.category !== tipo) {
+      return false;
+    }
+    if (combustivel && car.fuel !== combustivel) {
+      return false;
+    }
+    return true;
+  });
+
+
   return (
     <>
       <IonGrid>
         <IonRow>
-          {cars.map((car) => (
+          <IonCol>
+            <IonList>
+              <IonItem>
+                <IonSelect aria-label="tipo" placeholder="Tipo" onIonChange={(e) => setTipo(e.detail.value)}>
+                  <IonSelectOption value="SUV">SUV</IonSelectOption>
+                  <IonSelectOption value="Coupe">Coupe</IonSelectOption>
+                  <IonSelectOption value="utilitario">Utilitário</IonSelectOption>
+                </IonSelect>
+              </IonItem>
+            </IonList>
+            <IonList>
+              <IonItem>
+                <IonSelect aria-label="combustivel" placeholder="Combustível" onIonChange={(e) => setCombustivel(e.detail.value)}>
+                  <IonSelectOption value="gasolina">Gasolina</IonSelectOption>
+                  <IonSelectOption value="diesel">Gasóleo</IonSelectOption>
+                </IonSelect>
+              </IonItem>
+            </IonList>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
+      <IonGrid>
+        <IonRow>
+          {filteredCars.map((car) => (
             <IonCol sizeXl="4" sizeMd="6" sizeSm="6" sizeXs="12" key={car.id}>
               <Car car={car} />
             </IonCol>
--- src/components/ui/Car.tsx
+++ src/components/ui/Car.tsx
@@ -1,3 +1,5 @@
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from "@ionic/react";
+
 interface CarProps {
   id: number;
   name: string;
@@ -6,16 +8,24 @@ interface CarProps {
   price: string;
   price_day: string;
   year: string;
-  combustible: string;
-  store_id: string;
+  fuel: string;
+  store_id: number;
 }
 
 function Car({ car }: { car: CarProps }) {
   return (
-    <div>
-      <p> {car.name}</p>
-    </div>
+    <>
+      <IonCard color="light" >
+        <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+        <IonCardHeader>
+          <IonCardTitle>{car.name}</IonCardTitle>
+          <IonCardSubtitle></IonCardSubtitle>
+        </IonCardHeader>
+        <IonCardContent>
+        </IonCardContent>
+      </IonCard>
+
+    </>
   );
 }
-
 export default Car;
