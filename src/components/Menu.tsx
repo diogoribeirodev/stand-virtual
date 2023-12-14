bc546571c57710109b1356a2676022ae9c35ba63
Diogo Ribeiro <me@diogoribeiro.dev>
2023-12-14 10:52:25 +0000

    ...

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -4,10 +4,8 @@ import {
   IonItem,
   IonLabel,
   IonList,
-  IonListHeader,
   IonMenu,
-  IonMenuToggle,
-  IonNote,
+  IonMenuToggle
 } from '@ionic/react';
 
 import { carOutline, carSharp, homeOutline, homeSharp, storefrontOutline, storefrontSharp } from 'ionicons/icons';
@@ -49,8 +47,6 @@ const Menu: React.FC = () => {
     <IonMenu contentId="main" type="overlay">
       <IonContent>
         <IonList id="inbox-list">
-          <IonListHeader>Inbox</IonListHeader>
-          <IonNote>hi@ionicframework.com</IonNote>
           {appPages.map((appPage, index) => {
             return (
               <IonMenuToggle key={index} autoHide={false}>
